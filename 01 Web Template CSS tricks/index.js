0d615b3e44968ab0f00780afe5ccde74d60bbc47
Faxe37 <elias.fensholt.ahola@gmail.com>
2024-08-23 11:06:36 +0200

    fixed template javascript

--- 01 Web Template CSS tricks/index.js
+++ 01 Web Template CSS tricks/index.js
@@ -1,14 +1,30 @@
 //alert('hej Elias')
 
-let currentPage = 3
+let currentPage = 1
+let menuNumber = 1
+
 let pages //array med alle elementer med class = page
+let menuItems //array med alle menupunkterne
 let colors = ["blue","turqoise","purple","grey","cyan","hotpink"]
 
 function setup(){
+    select("#page" + currentPage).addClass ("visible")
+    select("#menu" + menuNumber).addClass ("active")
+
     pages = selectAll('.page')
+    menuItems = selectAll('.menuitem')
     //nu kan man se at pages er blevet til en liste med alle class = page ting
     //console.log(pages.length)
-    select('#page' + currentPage).addClass('visible')
+    for(m of menuItems){
+        m.mousePressed(function(e){
+            //e.tagret er selve html div'en
+            console.log(e.target.id)
+            //slice -1 henter det sidste bogstav i en string
+            let nr = e.target.id.slice(-1)
+            shiftPage(nr)
+
+        })
+    }
 
     //lav en masse div'er vi kommer ind i page 3
     for(c of colors){
@@ -32,6 +48,10 @@ function shiftPage(num){
     select("#page" + currentPage).removeClass ("visible")
     currentPage = num
     select("#page" + currentPage).addClass ("visible")
+
+    select("#menu" + menuNumber).removeClass ("active")
+    menuNumber = num
+    select("#menu" + menuNumber).addClass ("active")
 }
 
 function keyPressed(){
